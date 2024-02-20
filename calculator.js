0d0029c43bedd48767cd18a939d878e77d1629e9
Lauren Hughes <lauren_hughes@outlook.com.au>
2024-02-21 00:34:37 +1100

    Add roundSevenDP function
    Added the roundSevenDP function, which takes one number and returns that
    number rounded to seven decimal places. This was done to eliminate
    repeated code in the add, subtract, multiply and divide functions.

--- calculator.js
+++ calculator.js
@@ -1,17 +1,21 @@
 function add(num1, num2) {
-    return Math.round(((num1 + num2) + Number.EPSILON) * 10000000) / 10000000;
+    return roundSevenDP((num1 + num2));
 }
 
 function subtract(num1, num2) {
-    return Math.round(((num1 - num2) + Number.EPSILON) * 10000000) / 10000000;
+    return roundSevenDP((num1 - num2));
 }
 
 function multiply(num1, num2) {
-    return Math.round(((num1 * num2) + Number.EPSILON) * 10000000) / 10000000;
+    return roundSevenDP((num1 * num2));
 }
 
 function divide(num1, num2) {
-    return num2 === 0 ? "Nope" : Math.round(((num1 / num2) + Number.EPSILON) * 10000000) / 10000000;
+    return num2 === 0 ? "Nope" : roundSevenDP((num1 / num2));
+}
+
+function roundSevenDP(number) {
+    return Math.round((number + Number.EPSILON) * 10000000) / 10000000;
 }
 
 function operate(num1, num2, operator) {
